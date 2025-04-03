b350c1f030ae489129129c234a81f145ce291f8a
Dave Sommerville <dave.r.sommerville@outlook.com>
2025-04-02 21:13:11 -0500

    Appointment Service and Repository Added

--- Clinico.BLL/AppointmentService.cs
+++ Clinico.BLL/AppointmentService.cs
@@ -1,4 +1,43 @@
-﻿namespace Clinico.BLL {
-    public class AppointmentService {
+﻿using Clinico.Models;
+using Clinico.DAL;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Clinico.Model;
+
+namespace Clinico.BLL
+{
+    public class AppointmentService
+    {
+        private readonly AppointmentRepository _appointmentRepository;
+
+        public AppointmentService(AppointmentRepository appointmentRepository)
+        {
+            _appointmentRepository = appointmentRepository;
+        }
+
+        public async Task<List<Appointment>> GetAppointmentsAsync()
+        {
+            return await _appointmentRepository.GetAllAppointmentsAsync();
+        }
+
+        public async Task<Appointment> GetAppointmentByIdAsync(int id)
+        {
+            return await _appointmentRepository.GetAppointmentByIdAsync(id);
+        }
+
+        public async Task AddAppointmentAsync(Appointment appointment)
+        {
+            await _appointmentRepository.AddAppointmentAsync(appointment);
+        }
+
+        public async Task UpdateAppointmentAsync(Appointment appointment)
+        {
+            await _appointmentRepository.UpdateAppointmentAsync(appointment);
+        }
+
+        public async Task DeleteAppointmentAsync(int id)
+        {
+            await _appointmentRepository.DeleteAppointmentAsync(id);
+        }
     }
 }
--- Clinico.DAL/AppointmentRepository.cs
+++ Clinico.DAL/AppointmentRepository.cs
@@ -1,6 +1,55 @@
-﻿namespace Clinico.DAL {
-    public class AppointmentRepository {
-        // Testing
-        public string Test { get; set; } = "This is a test";
+﻿using Clinico.Model;
+using Clinico.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clinico.DAL
+{
+    public class AppointmentRepository
+    {
+        private readonly ClinicoContext _context;
+
+        public AppointmentRepository(ClinicoContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Appointment>> GetAllAppointmentsAsync()
+        {
+            return await _context.Appointments
+                .Include(a => a.Doctor)
+                .Include(a => a.Patient)
+                .Include(a => a.ExamRoom)
+                .ToListAsync();
+        }
+
+        public async Task<Appointment> GetAppointmentByIdAsync(int id)
+        {
+            return await _context.Appointments.FirstOrDefaultAsync(a => a.Id == id);
+        }
+
+        public async Task AddAppointmentAsync(Appointment appointment)
+        {
+            await _context.Appointments.AddAsync(appointment);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAppointmentAsync(Appointment appointment)
+        {
+            _context.Appointments.Update(appointment);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAppointmentAsync(int id)
+        {
+            var appointment = await _context.Appointments.FindAsync(id);
+            if (appointment != null)
+            {
+                _context.Appointments.Remove(appointment);
+                await _context.SaveChangesAsync();
+            }
+        }
     }
 }

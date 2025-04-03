50fc72b2fe486cf97abb9134080f28be778b8f65
Creeford Sanchez <creewrk@gmail.com>
2025-04-03 13:15:08 -0700

    Added API doctor CRUD

--- Clinico/Controllers/DoctorController.cs
+++ Clinico/Controllers/DoctorController.cs
@@ -1,9 +1,59 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Clinico.BLL;
+using Clinico.Model;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Clinico.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
     public class DoctorController : Controller {
-        public IActionResult Index() {
-            return View();
+        private readonly DoctorService _service;
+
+        public DoctorController(DoctorService service) {
+            _service = service;
+        }
+
+        [HttpGet("Get/{id}")]
+        public async Task<IActionResult> GetDoctor(int id) {
+            Doctor doctor = _service.GetDoctor(id);
+
+            if (doctor == null) return NotFound();
+
+            return Ok(doctor);
+        }
+
+        [HttpGet("Get")]
+        public async Task<IActionResult> GetDoctorList() {
+            List<Doctor> list = _service.GetDoctorList();
+
+            if (list == null) return NotFound();
+
+            return Ok(list);
+        }
+
+        [HttpPost("Create")]
+        public async Task<IActionResult> NewDoctor(Doctor doctor) {
+            if (doctor == null) return NotFound();
+
+            if (!ModelState.IsValid) return BadRequest();
+
+            return Ok();
+        }
+
+        [HttpPut("Edit/{id}")]
+        public async Task<IActionResult> EditDoctor(int id, Doctor doctor) {
+            if (id != doctor.Id || !ModelState.IsValid) return BadRequest();
+            _service.UpdateDoctor(doctor);          
+
+            return Ok();
+        }
+
+        [HttpDelete("Delete/{id}")]
+        public async Task<IActionResult>  DeleteDoctor(int id) {
+            Doctor doctor = _service.GetDoctor(id);
+
+            if (doctor == null) return NotFound();
+
+            return Ok();
         }
     }
 }

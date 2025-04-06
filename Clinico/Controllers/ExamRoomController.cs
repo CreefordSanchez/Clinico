b4a38e9e065a6a0b892fc6afb543421e2f37e627
Creeford Sanchez <creewrk@gmail.com>
2025-04-06 11:27:13 -0700

    Added API ExamRoom CRUD

--- Clinico/Controllers/ExamRoomController.cs
+++ Clinico/Controllers/ExamRoomController.cs
@@ -20,5 +20,31 @@ namespace Clinico.Controllers {
 
             return Ok(room);
         }
+
+        [HttpGet("Get")]
+        public async Task<IActionResult> GetExamRoomList() {
+            List<ExamRoom> list = await _service.GetExamRoomsList();
+
+            if (list == null) return NotFound();
+
+            return Ok(list);
+        }
+
+        [HttpPut("Edit/{id}")]
+        public async Task<IActionResult> EditDoctor(int id, ExamRoom room) {
+            if (id != room.Id || !ModelState.IsValid) return BadRequest();
+            await _service.UpdateExamRoom(room);
+
+            return Ok();
+        }
+
+        [HttpDelete("Delete/{id}")]
+        public async Task<IActionResult> DeletRoom(int id) {
+            ExamRoom room = await _service.GetExamRoom(id);
+
+            if (room == null) return NotFound();
+
+            return Ok();
+        }
     }
 }

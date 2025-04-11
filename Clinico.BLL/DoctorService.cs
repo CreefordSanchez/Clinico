73f5af4f0f4503434333e9f0fb574c93e5c28547
Creeford Sanchez <creewrk@gmail.com>
2025-04-10 20:14:57 -0700

    update DeleteDoctor (Delete appointment)

--- Clinico.BLL/DoctorService.cs
+++ Clinico.BLL/DoctorService.cs
@@ -4,9 +4,11 @@ using Clinico.Model;
 namespace Clinico.BLL {
     public class DoctorService {
         private readonly DoctorRepository _repository;
+        private readonly AppointmentService _appointmentService;
 
-        public DoctorService(DoctorRepository repository) {
+        public DoctorService(DoctorRepository repository, AppointmentService appointment) {
             _repository = repository;
+            _appointmentService = appointment;
         }
         public async Task CreateDoctor(Doctor doctor) {
             if (doctor != null) {
@@ -20,7 +22,14 @@ namespace Clinico.BLL {
         }
 
         public async Task DeleteDoctor(int id) {
-           await _repository.RemoveDoctor(id);            
+            List<Appointment> list = await _appointmentService.GetAppointmentsAsync();
+            List<Appointment> newList = list.Where(a => a.DoctorId == id).ToList();
+
+            foreach (Appointment appointment in newList) {
+                await _appointmentService.DeleteAppointmentAsync(appointment.Id);
+            }
+
+            await _repository.RemoveDoctor(id);            
         }
 
         public async Task<Doctor> GetDoctor(int id) {

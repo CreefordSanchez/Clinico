8d318997dbc65630b312bbd87dcef3ba0ed7d183
Creeford Sanchez <creewrk@gmail.com>
2025-04-11 18:07:34 -0700

    remove-appointment remove on doctor and room

--- Clinico.BLL/DoctorService.cs
+++ Clinico.BLL/DoctorService.cs
@@ -4,11 +4,9 @@ using Clinico.Model;
 namespace Clinico.BLL {
     public class DoctorService {
         private readonly DoctorRepository _repository;
-        private readonly AppointmentService _appointmentService;
 
-        public DoctorService(DoctorRepository repository, AppointmentService appointment) {
+        public DoctorService(DoctorRepository repository) {
             _repository = repository;
-            _appointmentService = appointment;
         }
         public async Task CreateDoctor(Doctor doctor) {
             if (doctor != null) {
@@ -22,13 +20,6 @@ namespace Clinico.BLL {
         }
 
         public async Task DeleteDoctor(int id) {
-            List<Appointment> list = await _appointmentService.GetAppointmentsAsync();
-            List<Appointment> newList = list.Where(a => a.DoctorId == id).ToList();
-
-            foreach (Appointment appointment in newList) {
-                await _appointmentService.DeleteAppointmentAsync(appointment.Id);
-            }
-
             await _repository.RemoveDoctor(id);            
         }
 
--- Clinico.BLL/ExamRoomService.cs
+++ Clinico.BLL/ExamRoomService.cs
@@ -4,21 +4,12 @@ using Clinico.Model;
 namespace Clinico.BLL {
     public class ExamRoomService {
         private readonly ExamRoomRepository _repository;
-        private readonly AppointmentService _appointmentService;
-        public ExamRoomService(ExamRoomRepository repository, AppointmentService appointmentService) {
+        public ExamRoomService(ExamRoomRepository repository) {
             _repository = repository;
-            _appointmentService = appointmentService;
 
         }
 
         public async Task RemoveExamRoom(int id) {
-            List<Appointment> list = await _appointmentService.GetAppointmentsAsync();
-            List<Appointment> newList = list.Where(a => a.RoomId == id).ToList();
-
-            foreach (Appointment appointment in newList) {
-                await _appointmentService.DeleteAppointmentAsync(appointment.Id);
-            }
-
             await _repository.RemoveExamRoom(id);         
         }
 
